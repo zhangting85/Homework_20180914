aa0a18ca5f0a5abbbecff0225713fe615661f566
iread9527 <2652929865@qq.com>
2019-03-08 22:29:59 +0800

    提交13.10大作业
    提交13.10大作业

---
+++ 13.10/rubin/test_rb.sh
@@ -0,0 +1,11 @@
+python3 currency_convert.py --help
+
+python3 currency_convert.py --field 2
+
+python3 currency_convert.py --field 2 --multiplier 0.8 -i data.csv -o data-fr.csv
+
+python3 currency_convert.py --field 2 --multiplier 1.25 -i data-fr.csv -o data.csv
+
+python3 currency_convert.py --field 2 --multiplier 0.8 < data.csv > data-fr.csv
+
+python3 currency_convert.py --field 2 --multiplier 1.25 < data-fr.csv > data.csv
